4211b93c99cd9ba2f8f5a326d7bd55ae764947e5
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-09-21 17:25:47 -0300

    Initial commit

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MainControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get main_index_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get main_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get main_contact_url
+    assert_response :success
+  end
+
+end
